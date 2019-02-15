1328f4f949439cfbad0d2663e8d7a7bbe9ea7632
AC Roselee <acroselee@gmail.com>
2019-02-15 11:37:02 -0800

    Backend API calls functioning as expected

--- database-mongodb/index.js
+++ database-mongodb/index.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 const mongoUri = 'mongodb://localhost/products';
-const autoIncrement = require('mongoose-auto-increment');
-
+const Product = require('./Product.js');
+const Pav = require('./Pav.js');
 const db = mongoose.connect(mongoUri, {useNewUrlParser: true});
 
+let fetchChartFromDB = () => {
+  let query = Product.find({}).limit(24);
+  return query;
+}
 
+let fetchFourRandomPAVsFromDB = ()=> {
+  let collection = Pav.aggregate(
+   [ { $sample: { size: 4 } } ]
+);
+return collection;
+}
 
-module.exports = db;
+module.exports.db = db;
+module.exports.fetchChartFromDB = fetchChartFromDB;
+module.exports.fetchFourRandomPAVsFromDB = fetchFourRandomPAVsFromDB;

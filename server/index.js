c276cad33acca5c71fcfc2247b0a4c195913dc54
AC Roselee <acroselee@gmail.com>
2019-02-23 11:58:09 -0800

    fixed .gitignore to read bundle.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const db = require('../database-mongodb/index.js');
 
 const app = express();
-const PORT = 8081;
+const PORT = process.env.PORT || 8081;
 
 
 app.use(express.static(path.join(__dirname, '../public')));

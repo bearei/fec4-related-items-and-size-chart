4db866830f5bc91baf7be6e366f15d4899bdcea4
wingzerocat <mikechenlou@gmail.com>
2019-03-09 07:53:18 -0600

    fixed endpoints

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const db = require('./db.js');
-
 const app = express();
+// const redis = require('redis');
+// const client = redis.createClient();
+
 const PORT = process.env.PORT || 8081;
 
 
 app.use('/:id', express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -21,14 +27,14 @@ app.delete('/delete/:id', (req, res) => {
     });
 });
 
-app.update('/update/:id', (req, res) => {
+app.put('/update/:id', (req, res) => {
   db.deletePAV(req.params.id, req.body)
     .then(() => {
       res.end('updated');
     });
 });
 
-app.post('/post/', (req, res) => {
+app.post('/post', (req, res) => {
   db.postPAV(req.body)
     .then(() => {
       res.end('posted');
@@ -46,6 +52,24 @@ app.get('/api/pavs/:id', (req, res) => {
   db.fetchFourRandomPAVsFromDB(req.params.id)
     .then(pavs => res.send(pavs));
 });
+
+// client.on('error', function(err) {
+//   console.log('error' + err);
+// });
+// const getCache = (req, res) => {
+//   client.get(req.params.id, (err, result) => {
+//     if (result) {
+//       res.send(result);
+//     } else {
+//       db.fetchFourRandomPAVsFromDB(req.params.id)
+//         .then(pavs => {
+//           client.setex(req.params.id, 60, JSON.stringify(pavs));
+//           res.send(pavs)
+//         });
+//     }
+//   })
+// }
+// app.get('/api/pavs/:id', getCache);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);

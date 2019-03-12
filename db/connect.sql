2f536de4f1e1559f29c26ce51fa132a87764375e
wingzerocat <mikechenlou@gmail.com>
2019-03-12 15:07:31 -0500

    add create database command

--- db/connect.sql
+++ db/connect.sql
@@ -1,3 +1,4 @@
+create database sdc;
 USE sdc;
 
 DROP TABLE IF EXISTS pavs;
@@ -9,26 +10,26 @@ CREATE TABLE sizes (bodyPart VARCHAR(20), size VARCHAR(30), measurement INT, id 
 CREATE TABLE pavs (link_to_image TEXT, star_rating INT, review_count INT, short_description TEXT, price DECIMAL, product_id INT, id INT auto_increment primary key);
 CREATE INDEX idx_product on pavs (product_id);
 
-LOAD DATA LOCAL INFILE 'db//s3test/fakePAVs0.csv'
+LOAD DATA LOCAL INFILE 'db/fakePAVs0.csv'
 INTO TABLE pavs
 FIELDS TERMINATED BY ',';
 
-LOAD DATA LOCAL INFILE 'db/s3test/fakePAVs1.csv'
+LOAD DATA LOCAL INFILE 'db/fakePAVs1.csv'
 INTO TABLE pavs
 FIELDS TERMINATED BY ',';
 
-LOAD DATA LOCAL INFILE 'db/s3test/fakePAVs2.csv'
+LOAD DATA LOCAL INFILE 'db/fakePAVs2.csv'
 INTO TABLE pavs
 FIELDS TERMINATED BY ',';
 
-LOAD DATA LOCAL INFILE 'db/s3test/fakePAVs3.csv'
+LOAD DATA LOCAL INFILE 'db/fakePAVs3.csv'
 INTO TABLE pavs
 FIELDS TERMINATED BY ',';
 
-LOAD DATA LOCAL INFILE 'db/s3test/fakeProducts.csv'
+LOAD DATA LOCAL INFILE 'db/fakeProducts.csv'
 INTO TABLE newproducts
 FIELDS TERMINATED BY ',';
 
-LOAD DATA LOCAL INFILE 'db/s3test/fakeSizeChart.csv'
+LOAD DATA LOCAL INFILE 'db/fakeSizeChart.csv'
 INTO TABLE sizes
 FIELDS TERMINATED BY ',';
